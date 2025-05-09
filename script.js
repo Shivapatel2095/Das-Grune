359654487ea5ef73c32a537c4f736ccd102a882f
Shivapatel2095 <shivapate2095@gmail.com>
2025-05-09 16:06:08 +0530

    Added new file or updated content

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const form = document.getElementById("form");
 
-    form.addEventListener("submit", function(e) {
-      e.preventDefault(); // Stop form from submitting normally
+form.addEventListener("submit", function(e) {
+  e.preventDefault(); // Stop form from submitting normally
 
-      const isConfirmed = confirm("Are you sure you want to submit the form?");
-      
-      if (isConfirmed) {
-        // Show success pop-up
-        alert("Form has been submitted successfully!");
-        
-        // Optionally clear the form
-        form.reset();
-      } else {
-        alert("Your form has been cancelled successfully!");
-        form.reset(); // Clear the form
-      }
-    });
+  const isConfirmed = confirm("Are you sure you want to submit the form?");
+
+  if (isConfirmed) {
+    alert("Form has been submitted successfully!");
+    form.reset(); // Optionally clear the form
+  } else {
+    alert("Your form has been cancelled successfully!");
+    form.reset(); // Clear the form
+  }
+});
